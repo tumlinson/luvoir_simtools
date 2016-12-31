67bf1d57ad350e673c8436a60d79f3f7cd461a9e
JT <tumlinson@stsci.edu>
2016-12-31 11:34:48 -0500

    WTF images

--- image_comparison/main.py
+++ image_comparison/main.py
@@ -2,9 +2,8 @@ import numpy as np
 import matplotlib.image as mpimg
 
 from bokeh.plotting import figure, gridplot, output_file, show
-from bokeh.models import ColumnDataSource, HBox, VBoxForm, HoverTool, Paragraph, Range1d
+from bokeh.models import ColumnDataSource, HBox, VBoxForm, Paragraph, Range1d
 from bokeh.io import hplot, vplot, curdoc
-from bokeh.models import HoverTool
 from bokeh.models.widgets import Select 
 
 image_to_use = Select(title="Image to See", value="Galaxy", options=["Galaxy (z=2)", "Deep Field", "Star Forming Region", "Perseus A", "Pluto"], width=200)
@@ -12,14 +11,14 @@ image_to_use = Select(title="Image to See", value="Galaxy", options=["Galaxy (z=
 image1 = "http://www.stsci.edu/~tumlinso/HDST_source_z2.jpg" 
 image2 = "http://www.stsci.edu/~tumlinso/HST_source_z2.jpg"
 
-p1 = figure(x_range=[0, 10], y_range=[-10, 0], tools="pan,reset,resize,save,box_zoom,wheel_zoom",toolbar_location='above', \
+p1 = figure(x_range=[0, 10], y_range=[-10, 0], tools="pan,resize,save,box_zoom,wheel_zoom,reset",toolbar_location='above', \
 			title='LUVOIR (12 m)')
 p1.background_fill_color = "white"
 p1.image_url(url=[image1], x=[0], y=[0], w=10, h=10)
 p1.x_range = Range1d(0, 10, bounds=(0, 10))  
 p1.y_range = Range1d(-10, 0, bounds=(-10,0))
 
-p3 = figure(x_range=[0, 10], y_range=[-10, 0], tools="pan,reset,resize,save,box_zoom,wheel_zoom",toolbar_location='above', \
+p3 = figure(x_range=[0, 10], y_range=[-10, 0], tools="pan,resize,save,box_zoom,wheel_zoom,reset",toolbar_location='above', \
 			title='Hubble')
 p3.y_range = p1.y_range 
 p3.x_range = p1.x_range 

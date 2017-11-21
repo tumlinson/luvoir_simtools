a3be4031f5ddace3bdc1af61590d96eb785df0db
Jason Tumlinson <tumlinson@lefty.local>
2017-11-21 16:26:23 -0500

    day's work on CMD

--- cmd/set_plot_options.py
+++ cmd/set_plot_options.py
@@ -18,11 +18,11 @@ def set_plot_options(plot):
 
     plot.axis.visible = False # use this to flip the axis labeling on and off 
 
-    y_label_source = ColumnDataSource(data={'x': [-1.5,-1.5,-1.5,-1.5,-1.5],
+    y_label_source = ColumnDataSource(data={'x': [-1.3,-1.3,-1.3,-1.3,-1.3],
                                         'y': [-10-0.4,-5-0.4,0-0.4,5-0.4,10-0.4], 'text':['10','5','0','-5','-10']})
     x_label_source = ColumnDataSource(data={'x': [-1.0,0.0,1.0,2.0, 3.0],
-                                        'y': [-13.5-0.2,-13.5-0.2,-13.5-0.2,-13.5-0.2,-13.5-0.2], 'text':['-1','0','1.0','2','3']})
-    plot.text('x','y','text', source=x_label_source, text_align='center', text_font_size='15pt', text_color='deepskyblue')
-    plot.text('x','y','text', source=y_label_source, text_font_size='15pt', text_color='deepskyblue', text_align='right')
-    plot.quad(top=[7], bottom=[-13.0], left=[-1.2], right=[3.2], fill_alpha=0, line_width=2, line_color='black') 
+                                        'y': [-13.5-0.2,-13.5-0.2,-13.5-0.2,-13.5-0.2,-13.5-0.2], 'text':['-1','0','1','2','3']})
+    plot.text('x','y','text', source=x_label_source, text_align='center', text_font_size='15pt', text_color='black')
+    plot.text('x','y','text', source=y_label_source, text_font_size='15pt', text_color='black', text_align='right')
+    plot.quad(top=[7], bottom=[-13.0], left=[-1.2], right=[3.2], fill_alpha=0, line_width=1, line_color='black') 
 
